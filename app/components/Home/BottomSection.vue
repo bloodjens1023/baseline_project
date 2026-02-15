a4d182a47738474dea865af469cbce4d3d5cc1b6
unknown <arsenefaly1@gmail.com>
2026-02-15 20:18:14 +0300

    Section Bottom

---
+++ app/components/Home/BottomSection.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <!-- Les 9 Icons  -->
+    <div class="px-8 pt-8">
+      <div class="flex justify-center items-center gap-2 mb-2">
+        <div class="share-icon" style="width: 400px">
+          <img
+            src="https://downpg117.uqianbao.com/source/public/static/svg/GZ2uwZ4U.svg"
+            alt=""
+            class="w-full rounded-lg"
+          />
+        </div>
+        <div class="share-icon" style="width: 400px">
+          <img
+            src="https://downpg117.uqianbao.com/source/public/static/svg/Civ14LDW.svg"
+            alt=""
+            class="w-full rounded-lg"
+          />
+        </div>
+        <div class="share-icon" style="width: 400px">
+          <img
+            src="https://downpg117.uqianbao.com/source/public/static/svg/B6TXnIDj.svg"
+            alt=""
+            class="w-full rounded-lg"
+          />
+        </div>
+      </div>
+
+      <div class="flex justify-center items-center gap-2 mb-2">
+        <div class="share-icon" style="width: 400px">
+          <img
+            src="https://downpg117.uqianbao.com/source/public/static/svg/rP1oFiSh.svg"
+            alt=""
+            class="w-full rounded-lg"
+          />
+        </div>
+        <div class="share-icon" style="width: 400px">
+          <img
+            src="https://downpg117.uqianbao.com/source/public/static/svg/D_i-KO_s.svg"
+            alt=""
+            class="w-full rounded-lg"
+          />
+        </div>
+        <div class="share-icon" style="width: 400px">
+          <img
+            src="https://downpg117.uqianbao.com/source/public/static/svg/CDma3emR.svg"
+            alt=""
+            class="w-full rounded-lg"
+          />
+        </div>
+      </div>
+
+      <div class="flex justify-center items-center gap-2 mb-2">
+        <div class="share-icon" style="width: 400px">
+          <img
+            src="https://downpg117.uqianbao.com/source/public/static/svg/B5U0gd8x.svg"
+            alt=""
+            class="w-full rounded-lg"
+          />
+        </div>
+        <div class="share-icon" style="width: 400px">
+          <img
+            src="https://downpg117.uqianbao.com/source/public/static/svg/BVy-OIx7.svg"
+            alt=""
+            class="w-full rounded-lg"
+          />
+        </div>
+        <div class="share-icon" style="width: 400px">
+          <img
+            src="https://downpg117.uqianbao.com/source/public/static/svg/CllcxtMJ.svg"
+            alt=""
+            class="w-full rounded-lg"
+          />
+        </div>
+      </div>
+    </div>
+
+    <!-- Last section -->
+    <div
+      class="text-center w-full flex flex-col justify-center items-center text-[12px] gap-1 mt-10 font-bold"
+    >
+      <span>Copyright © 2015 – 2025 giro77.bet. All rights reserved.</span>
+      <div class="w-full flex justify-center items-center">
+        updateTime: 2025-12-03 08:30:53
+      </div>
+    </div>
+
+    <!-- Back to Top Button -->
+    <div class="w-full flex justify-center mt-10">
+      <div class="el-backtop" style="right: 40px; bottom: 40px">
+        <div
+          class="flex flex-col justify-center items-center text-center text-white h-full"
+        >
+          <img
+            src="https://downpg117.uqianbao.com/source/public/static/svg/DSrEP2nO.svg"
+            alt="Back to top"
+            class="w-[120px] rounded-lg"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
