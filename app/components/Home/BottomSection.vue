ce2d43ca9f6e2de1b8ce3cbf3fece7ccd6cb5941
unknown <arsenefaly1@gmail.com>
2026-02-15 20:50:45 +0300

    Final Bottom section

--- app/components/Home/BottomSection.vue
+++ app/components/Home/BottomSection.vue
@@ -1,7 +1,148 @@
 <template>
   <div>
     <div
-      class="w-full flex flex-row justify-between items-center gap-[8px] px-6 mt-4"
+      class="text-center w-full flex justify-center items-center text-[12px] font-bold px-10 pt-20"
+    >
+      Bem-vindo(a) à comunidade giro77.bet! Aqui compartilhamos ideias,
+      discutimos temas interessantes e interagimos com respeito. Participe! 😊
+    </div>
+
+    <div
+      class="w-full flex flex-row justify-center gap-[10px] rounded-lg mt-6"
+    >
+      <div
+        class="flex flex-row justify-center items-center rounded-lg bg-gradient-to-b from-[#162d77] to-[#1f4789] px-4 py-2 gap-2"
+      >
+        <img
+          src="https://downpg117.uqianbao.com/source/public/static/svg/kN_fe_Gl.svg"
+          alt="Telegram Icon"
+          loading="eager"
+          fetchpriority="high"
+          decoding="async"
+          class="h-7 w-7"
+        />
+        <span class="text-white text-xs font-bold">Telegram Channel</span>
+      </div>
+    </div>
+
+    <p class="text-center text-[12px] font-bold mt-6">
+      Contact us: support@giro77.bet
+    </p>
+
+    <!-- list icons -->
+    <div class="share flex w-full gap-[10px] justify-center mt-6">
+      <div class="share-icon w-[60px] h-[60px] rounded-lg flex justify-center items-center">
+        <img
+          src="https://downpg117.uqianbao.com/source/public/static/svg/BpjPtDpL.svg"
+          alt=""
+          loading="eager"
+          fetchpriority="high"
+          decoding="async"
+          class="h-[45px]"
+        />
+      </div>
+      <div class="share-icon w-[60px] h-[60px] rounded-lg flex justify-center items-center">
+        <img
+          src="https://downpg117.uqianbao.com/source/public/static/svg/kN_fe_Gl.svg"
+          alt=""
+          loading="eager"
+          fetchpriority="high"
+          decoding="async"
+          class="h-[45px] logo"
+        />
+      </div>
+      <div class="share-icon w-[60px] h-[60px] rounded-lg flex justify-center items-center">
+        <img
+          src="https://downpg117.uqianbao.com/source/public/static/svg/B_Gx7ck-.svg"
+          alt=""
+          loading="eager"
+          fetchpriority="high"
+          decoding="async"
+          class="h-[45px] logo"
+        />
+      </div>
+      <div class="share-icon w-[60px] h-[60px] rounded-lg flex justify-center items-center">
+        <img
+          src="https://downpg117.uqianbao.com/source/public/static/svg/URMDl3hM.svg"
+          alt=""
+          loading="eager"
+          fetchpriority="high"
+          decoding="async"
+          class="h-[45px] logo"
+        />
+      </div>
+      <div class="share-icon w-[60px] h-[60px] rounded-lg flex justify-center items-center">
+        <img
+          src="https://downpg117.uqianbao.com/source/public/static/svg/oMuAMAQ6.svg"
+          alt=""
+          loading="eager"
+          fetchpriority="high"
+          decoding="async"
+          class="h-[45px] logo"
+        />
+      </div>
+    </div>
+
+    <!-- Lien -->
+    <div class="w-full flex justify-center mt-6">
+      <div class="w-[90%] max-w-4xl">
+        <div class="flex justify-center gap-3 flex-wrap">
+          <button
+            aria-disabled="false"
+            type="button"
+            class="text-sm font-bold text-white"
+          >
+            How To Play
+          </button>
+          <button
+            aria-disabled="false"
+            type="button"
+            class="text-sm font-bold text-white"
+          >
+            Terms & Conditions
+          </button>
+          <button
+            aria-disabled="false"
+            type="button"
+            class="text-sm font-bold text-white"
+          >
+            AML Policy
+          </button>
+          <button
+            aria-disabled="false"
+            type="button"
+            class="text-sm font-bold text-white"
+          >
+            KYC Policy
+          </button>
+          <button
+            aria-disabled="false"
+            type="button"
+            class="text-sm font-bold text-white"
+          >
+            Self Exclusion Policy
+          </button>
+          <button
+            aria-disabled="false"
+            type="button"
+            class="text-sm font-bold text-white"
+          >
+            Responsible Gaming Policy
+          </button>
+          <button
+            aria-disabled="false"
+            type="button"
+            class="text-sm font-bold text-white"
+          >
+            Underage Gaming Policy (18+ to play clause)
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <!-- Title image -->
+    <div
+      class="w-full flex flex-row justify-between items-center gap-[8px] px-6 mt-14"
     >
       <img
         src="https://downpg117.uqianbao.com/source/public/static/webp/CbkHlecm.webp"
@@ -30,7 +171,7 @@
     </div>
 
     <!-- Desc -->
-    <div class="Contact w-full flex flex-col text-[12px] px-8 mt-4 font-bold">
+    <div class="Contact w-full flex flex-col text-[12px] px-8 mt-6 font-bold">
       <div class="company-introduction leading-[16px] break-all">
         giro77.bet é uma marca administrada por GAMEWIZ BRASIL LTDA CNP.Ino 56
         195 099/0001-89, devidamente autorizada pelo Ministério da Fazenda sob a
@@ -47,7 +188,7 @@
     </div>
 
     <!-- Les 9 Icons  -->
-    <div class="px-8 pt-8">
+    <div class="px-8 pt-8 mt-8">
       <div class="flex justify-center items-center gap-2 mb-2">
         <div class="share-icon" style="width: 400px">
           <img
@@ -132,7 +273,7 @@
     </div>
 
     <!-- Back to Top Button -->
-    <div class="w-full flex justify-center mt-10 mb-10">
+    <div class="w-full flex justify-center mt-20 mb-10">
       <div class="el-backtop" style="right: 40px; bottom: 40px">
         <div
           class="flex flex-col justify-center items-center text-center text-white h-full"
