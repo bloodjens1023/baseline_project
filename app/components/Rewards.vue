fc14a19b98911744ac327fb34a5c2af2ba377458
bloodjens1023 <bloodjens32@gmail.com>
2026-02-15 16:47:28 +0000

    creation of components rewards

---
+++ app/components/Rewards.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div 
+    class="flex flex-1 h-full bg-cover"
+    :style="{ backgroundImage: `url('${bgImage}')` }"
+  >
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  bgImage: {
+    type: String,
+    required: true
+  }
+})
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,7 +2,13 @@
   <div>
     <img class="w-full h-full object-cover p-3"
       src="https://downpg117.uqianbao.com/source/public/static/webp/DKG9OB7h.webp" alt="">
-      
+  </div>
+  
+  <div class="w-full px-[10px] mt-[10px]">
+    <div class="w-full aspect-[6.73/1] flex flex-row justify-center items-center gap-[10px] ">
+      <Rewards bg-image="https://downpg117.uqianbao.com/source/public/static/webp/sdfBvLKb.webp"/>
+      <Rewards bg-image="https://downpg117.uqianbao.com/source/public/static/webp/DMpFmNoL.webp"/>
+    </div>
   </div>
 </template>
 <script setup>
