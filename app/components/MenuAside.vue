e89d8ea7b17848e33fde473bc603d1254c63ca76
unknown <arsenefaly1@gmail.com>
2026-02-16 15:31:24 +0300

    Implementation MenuAside

--- app/components/MenuAside.vue
+++ app/components/MenuAside.vue
@@ -8,18 +8,24 @@
         <div class="max-w-[500px] mx-auto h-full relative">
           <aside
             @click.stop
-            class="absolute left-0 top-0 h-full w-80 bg-gradient-to-b from-[#162d77] to-[#1f4789] shadow-2xl overflow-y-auto"
+            class="absolute left-0 top-0 h-full w-[70%] bg-[#061237] shadow-2xl overflow-y-auto scrollbar-none"
           >
-            <!-- Header avec bouton fermer et logo -->
+            <!-- Header fixe -->
             <div
-              class="px-4 py-4 border-b border-blue-800/30 flex items-center justify-between"
+              class="sticky top-0 z-10 w-full bg-gradient-to-b from-[#162d77] to-[#1f4789] backdrop-blur-sm text-white shadow-xl border-b border-blue-800/30 px-4 py-4 flex items-center justify-between"
+              style="
+                background-image:
+                  url(&quot;https://downpg117.uqianbao.com/source/public/static/webp/OMR6_kmW.webp&quot;),
+                  url(&quot;https://downpg117.uqianbao.com/source/public/static/webp/OMR6_kmW.webp&quot;);
+                background-position:
+                  left top,
+                  right top;
+                background-repeat: no-repeat, no-repeat;
+                background-size: cover;
+              "
             >
-              <img
-                src="/asset/svg/logo.svg"
-                alt=""
-                srcset=""
-                class="w-[120px]"
-              />
+              <img src="/asset/svg/logo.svg" alt="" class="w-[120px]" />
+
               <button
                 @click="handleClose"
                 class="p-2 bg-blue-800/50 rounded-xl hover:bg-blue-700/50 transition-all duration-200"
@@ -28,10 +34,10 @@
                 <img
                   src="https://downpg117.uqianbao.com/source/public/static/svg/XVrMH9vx.svg"
                   alt="Close"
-                  class=""
                 />
               </button>
             </div>
+            <menu-aside-content />
           </aside>
         </div>
       </div>
@@ -40,6 +46,8 @@
 </template>
 
 <script setup>
+import MenuAsideContent from './MenuAsideContent.vue';
+
 defineProps({
   isOpen: {
     type: Boolean,
@@ -68,5 +76,14 @@ const handleClose = () => {
 .slide-enter-to aside,
 .slide-leave-from aside {
   transform: translateX(0);
+}
+
+/* Masquer la scrollbar */
+.scrollbar-none::-webkit-scrollbar {
+  display: none; /* Chrome, Safari, Opera */
+}
+.scrollbar-none {
+  -ms-overflow-style: none; /* IE et Edge */
+  scrollbar-width: none; /* Firefox */
 }
 </style>
---
+++ app/components/MenuAsideContent.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="p-3">
+    <div class="w-full flex flex-col gap-2">
+      <div
+        v-for="(item, index) in items"
+        :key="index"
+        class="w-full flex justify-between items-center py-[0.4rem] px-3 bg-[#15194c] rounded-lg border border-blue-900/30"
+      >
+        <!-- Left item -->
+        <div class="flex items-center gap-3">
+          <div class="p-1.5">
+            <img
+              :src="item.icon"
+              :alt="item.title"
+              class="w-6 h-6"
+              loading="eager"
+              decoding="async"
+            />
+          </div>
+          <span class="text-sm font-bold text-blue-400">
+            {{ item.title }}
+          </span>
+        </div>
+
+        <!-- Right SVG -->
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="w-10 h-6 text-blue-400/80 bg-blue-950/70 rounded-md p-1"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9 5l7 7-7 7"
+          />
+        </svg>
+      </div>
+    </div>
+
+    <!-- Bouton principal -->
+    <button
+      class="w-full mt-3 bg-gradient-to-b from-[#bfea36] to-[#48bc49] active:from-emerald-700 active:via-green-700 active:to-teal-700 text-black font-bold py-[0.7rem] px-5 rounded-xl shadow-xl border border-emerald-400/30 transition-all duration-200 mb-6"
+    >
+      Ganhe R$100 de graça
+    </button>
+
+    <!-- Grid 3x3 -->
+    <div class="grid grid-cols-3 gap-3 text-white font-bold mb-14">
+      <RoundedButton
+        v-for="(btn, index) in buttons"
+        :key="index"
+        :title="btn.title"
+        :bg-image="`${config.public.assetsURL}${btn.image}`"
+      />
+    </div>
+
+    <div class="w-full flex flex-col gap-2 mb-5">
+      <div
+        v-for="(item, index) in items2"
+        :key="index"
+        class="w-full flex justify-between items-center py-[0.4rem] px-3 bg-[#15194c] rounded-lg border border-blue-900/30"
+      >
+        <!-- Left item -->
+        <div class="flex items-center gap-3">
+          <div class="p-1.5">
+            <img
+              :src="item.icon"
+              :alt="item.title"
+              class="w-6 h-6"
+              loading="eager"
+              decoding="async"
+            />
+          </div>
+          <span class="text-sm font-bold text-blue-400">
+            {{ item.title }}
+          </span>
+        </div>
+
+        <!-- Right SVG -->
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="w-10 h-6 text-blue-400/80 bg-blue-950/70 rounded-md p-1"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9 5l7 7-7 7"
+          />
+        </svg>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const config = useRuntimeConfig();
+
+const buttons = [
+  { title: "Resgatar", image: "/static/webp/C7_SoLrK.webp" },
+  { title: "Semana", image: "/static/webp/3TY0Tzzx.webp" },
+  { title: "Misterioso", image: "/static/webp/D9Vf61xP.webp" },
+  { title: "Check-in", image: "/static/webp/BzOmGKPn.webp" },
+  { title: "Roleta", image: "/static/webp/CHoWiKXg.webp" },
+  { title: "Roleta", image: "/static/webp/CHoWiKXg.webp" },
+  { title: "Check-in", image: "/static/webp/BzOmGKPn.webp" },
+  { title: "Misterioso", image: "/static/webp/D9Vf61xP.webp" },
+  { title: "Resgatar", image: "/static/webp/C7_SoLrK.webp" },
+];
+
+const items = [
+  {
+    title: "Depósito",
+    icon: "https://downpg117.uqianbao.com/source/public/static/svg/vBJGo5rR.svg",
+  },
+  {
+    title: "Saque",
+    icon: "https://downpg117.uqianbao.com/source/public/static/svg/vBJGo5rR.svg",
+  },
+  {
+    title: "Código De Bônus",
+    icon: "https://downpg117.uqianbao.com/source/public/static/svg/vBJGo5rR.svg",
+  },
+];
+
+const items2 = [
+  {
+    title: "Mensagem",
+    icon: "https://downpg117.uqianbao.com/source/public/static/svg/IWi2FDed.svg",
+  },
+  {
+    title: "Adiconar à tela inicial",
+    icon: "https://downpg117.uqianbao.com/source/public/static/svg/CcaUXxLJ.svg",
+  },
+  {
+    title: "Suporte ao vivo",
+    icon: "https://downpg117.uqianbao.com/source/public/static/svg/DrVQ8Lpq.svg",
+  },
+];
+</script>
